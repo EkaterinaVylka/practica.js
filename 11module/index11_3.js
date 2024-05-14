93d79b5f4081387c48620b0a49bcce95724a06c2
Ekaterina <Ytaka123@yandex.ru>
2024-05-15 00:04:15 +0300

    первая фиксация

--- 11module/index11_3.js
+++
@@ -1,7 +0,0 @@
-function getSum(num1) {
-    return function (num2) {
-        return num1 + num2;
-    }
-}
-
-console.log(getSum(1)(3));
--- 11module/index11_5.js
+++
@@ -1,9 +0,0 @@
-
-function exponentiate(x, n) {
-    console.log(x ** n);
-}
-exponentiate(4, 4);
-
-
-const exp = (x, n) => console.log(x ** n);
-exp(4, 4);
